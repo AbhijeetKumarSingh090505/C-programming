a266fc976586cb5b4dcebad5d46c969f3d8f6092
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-10 22:40:29 +0530

    swaping without third variable and +,-operator

---
+++ A05.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("enter b;");
+    scanf("%d",&b);
+    printf("values before swap\n a=%d,b=%d\n\n",a,b);
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    printf("values after swap\n a=%d,b=%d\n\n",a,b);
+}

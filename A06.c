c467dde6338c1e8077b3ebeb39b8f4f3de39f62c
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-11 22:10:26 +0530

    display the details given by the user and calculate age

---
+++ A06.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char a,b,c;
+    int d;
+    printf("Enter name:\n");
+    scanf("%c",&a);
+    printf("enter birthyear:");
+    scanf("%d",&d);
+    printf("enter program:");
+    scanf("%C",&b);
+    printf("Enter mail id:");
+    scanf("%c",&c);
+    printf("age=%d",2023-d);
+}

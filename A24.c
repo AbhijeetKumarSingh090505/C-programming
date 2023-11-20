5869b393feb97278f102e0de4f914c62fda443b8
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-20 07:59:37 +0530

    beep sound produce

---
+++ A24.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+    printf("\a");
+}

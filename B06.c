99919b456163f49881e0726fe1bf942c632c57d8
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-23 23:29:35 +0530

    area of triangle using heron's formula

---
+++ B06.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){float a,b,c,d,s,ar;
+    printf("enter side a:");
+    scanf("%f",&a);
+    printf("enter side b:");
+    scanf("%f",&b);
+    printf("enter side c:");
+    scanf("%f",&c);
+    s=(a+b+c)/2;
+    ar= sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("area of the triangle:%f",ar);
+
+
+}

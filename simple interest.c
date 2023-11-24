335c502c275ad41facec637942b4504179fb0528
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-24 22:18:51 +0530

    simple interest

---
+++ simple interest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float si,p,r,t;
+    printf("enter side p:");
+    scanf("%f",&p);
+    printf("enter side r:");
+    scanf("%f",&r);
+    printf("enter side t:");
+    scanf("%f",&t);
+    si=(p*r*t)/100;
+    printf("si=%f",si);
+}

ef9072bf3591b539a0cf8f9fd1d67e82fbfb9cbb
Abhijeet Kumar Singh <147933501+AbhijeetKumarSingh090505@users.noreply.github.com>
2023-11-28 23:18:42 +0530

    check number is prime or composite

---
+++ check prime or composite.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,m,a=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    m=n/2;
+    for(int i=2;i<=m;i++){
+        if(n%i==0){
+            printf("%d is not a prime number",n);
+            a=1;
+            break;
+        }
+    }
+    if(a==0){
+        printf("%d is a prime number it is a composite number",n);
+    }
+}
